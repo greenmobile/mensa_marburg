fae2182ac625dcc9b15fcf03c8cbc5b35ca9d718
gmino <heigel@gmino.de>
2012-11-09 13:15:12 +0100

    added database model

---
+++ Marburg Mensa/Marburg Mensa/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  Marburg Mensa
+//
+//  Created by heigel on 09.11.12.
+//  Copyright (c) 2012 greenmobile Innovations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
---
+++ Marburg Mensa/Marburg MensaTests/Marburg_MensaTests.h
@@ -0,0 +1,13 @@
+//
+//  Marburg_MensaTests.h
+//  Marburg MensaTests
+//
+//  Created by heigel on 09.11.12.
+//  Copyright (c) 2012 greenmobile Innovations. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Marburg_MensaTests : SenTestCase
+
+@end
